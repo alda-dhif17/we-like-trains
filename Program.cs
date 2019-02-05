bf84cad9cb31c8307db00a17a8ae66a3d539fdfa
TheCrether <denisimeri6@gmail.com>
2019-02-05 08:28:59 +0100

    Strings werden generiert
    Strings werden mit eingegebener Länge generiert und dann werden alle doppelten Zeichen entfernt

--- Program.cs
+++ Program.cs
@@ -29,19 +29,46 @@ namespace Gueterzug
                 return tset;
             }
         }
-        static void Main(string[] args) {
-            List<char> inputList = "ahdzempz".ToCharArray().ToList();
-            IEnumerable<Char> inputEnum = inputList.Distinct();
-            String input = "";
 
-            foreach(char ch in inputEnum)
+        public static String genString(int length)
+        {
+            String str = "";
+            Random rand = new Random();
+
+            for(int i = 0;i < length; i++)
             {
-                input += ch;
+                int choice = rand.Next(0, 2);
+
+                if(choice == 0)
+                {
+                    str += (char)(rand.Next('a', 'z'));
+                }
+                else
+                {
+                    str += (char)(rand.Next('A', 'Z'));
+                }
             }
 
+            char[] chArray = str.ToCharArray();
+            IEnumerable<Char> chEnum = chArray.Distinct();
+            str = "";
+            foreach(char c in chEnum)
+            {
+                str += c;
+            }
+
+            return str;
+        }
+        static void Main(string[] args) {
+            Console.Write("Länge (ist nicht unbedingt finale Länge, weil nicht doppeltes vorkommen soll): ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            String input = genString(length);
+            Console.WriteLine("\nGenerierte Zeichenfolge: {0}\n" +
+                "Länge der Zeichenfolge {1}", input, input.Length);
 
             String output = ef(input, 0);
-            Console.WriteLine(output);
+            Console.WriteLine("\nSortierte Zeichenfolge (Großbuchstaben kommen zuerst): {0}",output);
             Console.ReadLine();
         }
     }
